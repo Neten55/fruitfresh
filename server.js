5d611bd1afc8010667e359656cef20a6da5bf32a
Neten-ND <https://github.com/Neten-ND/Practical1.git>
2025-07-11 02:53:58 -0700

    fix: Correct syntax error in server startup log

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ mongoose.connect(process.env.MONGO_URI)
 
         // Start server only after MongoDB connection is established
         app.listen(PORT, () => {
-            console.log(Server is running on port ${PORT});
+            console.log(`Server is running on port ${PORT}`);
         });
     })
     .catch((err) => {
